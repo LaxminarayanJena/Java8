915d03b6f5fc6304960c92abe54db4636d3a63d5
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-09-01 20:17:45 +0530

    PrimeNumberWithParallelStream

--- src/streamsFlatMap/ParallelStreams.java
+++ src/streamsFlatMap/ParallelStreams.java
@@ -1,6 +1,8 @@
 package streamsFlatMap;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -33,7 +35,40 @@ public class ParallelStreams {
 		IntStream.rangeClosed(1,10).forEach(System.out::println);
 		System.out.println("--------parallel stream-----");
 		IntStream.rangeClosed(1, 10).parallel().forEach(System.out::println);
+		
+		//to print fro a-z
+		System.out.println(getAlphabetsList());
+		System.out.println("-----normal stream---");
+		getAlphabetsList().stream().forEach(System.out::println);
+		System.out.println("--------parallel stream-----");
+		getAlphabetsList().parallelStream().forEach(System.out::println);
+		
+		
+		//check stream is running in parallel mode :isParallel() method:
+		IntStream range =IntStream.range(1, 10);
+		System.out.println(range.isParallel()); //false
+		
+		IntStream range1 =IntStream.range(1, 10);
+		range1.parallel();
+		System.out.println(range1.isParallel()); //true
+		
+		
+		
 
+	}
+	
+	public static List<String> getAlphabetsList()
+	{
+		List<String> alpha = new ArrayList<String>();
+		int n=97;
+		while(n<=122)
+		{
+			char c=(char) n;
+			alpha.add(String.valueOf(c));
+			n++;
+		}
+		return alpha;
+		
 	}
 
 }
